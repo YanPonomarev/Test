e43f1f099bbcab3c025fe0471557941fc4c4b9d2
Yan Ponomarev <yanponomarev@gmail.com>
2021-08-22 00:56:31 +0300

    Handlеbars тестируем

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-import './main.scss'
+const Handlebars = require("handlebars");
 
-document.getElementById('dog').innerText = 'Привет первая сборка!'
+const source = document.querySelector("#introduction-template").innerHTML;
+
+const template = Handlebars.compile(source);
+
+const context = {
+    name: "Vlada"
+};
+
+const html = template(context);
+
+const destination = document.querySelector("#introduction");
+
+destination.innerHTML = html;
