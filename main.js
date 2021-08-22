73b2bc01614b4a9b7cc209d6b5cd925fbe7bc77c
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2021-08-22 20:11:50 +0300

    Фундаментальные изменения handlebars
    Вёрстка за счет шаблонизатора,
    стили накинуты через scss,
    в main.js разбор шаблона
    (использовать в качестве шаблона при работе)

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ const source = document.querySelector("#introduction-template").innerHTML;
 const template = Handlebars.compile(source);
 
 const context = {
-    name: "Vlada"
+    marker: false,
+    variants: {
+        variant_1: "variant 1",
+        variant_2: "variant 2"
+    },
+    people: [
+        'Vlada',
+        'Yan',
+        'sobachka',
+        'koshka'
+    ]
 };
 
 const html = template(context);
@@ -13,3 +23,8 @@ const html = template(context);
 const destination = document.querySelector("#introduction");
 
 destination.innerHTML = html;
+
+
+// Handlebars.registerHelper('isdefined', function (value) {
+//   return value !== false;
+// });
